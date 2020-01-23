d011adca126989748482b4ab4d79593c97191b89
Bryce Meyer <bryce13950@gmail.com>
2020-01-24 02:37:10 +0400

    imported base home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,33 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: './pages/home/home.module#HomePageModule'
+  },
+  {
+    path: 'threads',
+    loadChildren: './pages/threads/threads.module#ThreadsPageModule',
+  },
+  {
+    path: 'user/:user_id',
+    loadChildren: './pages/user/user.module#UserPageModule',
+  },
+  // {
+  //   path: 'contacts',
+  //   loadChildren: './pages/contacts/contacts.module#ContactsPageModule',
+  // },
+  {
+    path: 'user/:user_id/message',
+    loadChildren: './pages/thread/thread.module#ThreadPageModule',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {BasePage} from '../base.page';
+
+/**
+ * Main home page of the app
+ */
+@Component({
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
+})
+export class HomePage extends BasePage {
+
+}

9695fd410faed082e5e2cd1a13d70cfd0c3c899a
Bryce Meyer <bryce13950@gmail.com>
2020-10-29 20:22:38 +0100

    tested subscription page a bit

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,34 +1,61 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthManagerService} from '../../services/auth-manager/auth-manager.service';
+import {environment} from '../../../environments/environment';
 
 @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
     styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
-
+export class MenuComponent
+{
     /**
      * Default Constructor
      * @param router
      * @param authManagerService
      */
     constructor(private router: Router,
-                private authManagerService: AuthManagerService) {
-    }
+                private authManagerService: AuthManagerService)
+    {}
 
     /**
      * Goes to the home page
      */
-    goTo(url: string) {
+    goTo(url: string)
+    {
         this.router.navigateByUrl(url).catch(console.error);
     }
 
     /**
      * Runs the logout
      */
-    logout() {
+    logout()
+    {
         this.authManagerService.logOut();
+    }
+
+    /**
+     * Tells us whether or not this particular app has the social media components enabled
+     */
+    isSocialMediaEnabled()
+    {
+        return environment.social_media_enabled;
+    }
+
+    /**
+     * Tells us whether or not this particular app has individual subscriptions enabled
+     */
+    areSubscriptionsEnabled()
+    {
+        return environment.subscriptions_enabled;
+    }
+
+    /**
+     * Tells us whether or not this particular app has the organization components enabled
+     */
+    areOrganizationsEnabled()
+    {
+        return environment.organizations_enabled;
     }
 }

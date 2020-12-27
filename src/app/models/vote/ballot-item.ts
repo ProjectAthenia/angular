d06f72d0e1667da0f2c41e713a9ea17b1e05dc4a
Bryce Meyer <bryce13950@gmail.com>
2020-12-27 17:01:18 +0100

    added vote models

---
+++ src/app/models/vote/ballot-item.ts
@@ -0,0 +1,24 @@
+import {BaseModel} from '../base-model';
+import {BallotItemOption} from './ballot-item-option';
+import {Relation} from '../relation';
+
+/**
+ * Used as a data wrapper for our ballot model
+ */
+export class BallotItem extends BaseModel {
+
+    /**
+     * The options for this ballot item
+     */
+    ballot_item_options: BallotItemOption[];
+
+    /**
+     * Default constructor
+     * @param data
+     */
+    constructor(data) {
+        super(data, {
+            ballot_item_options: new Relation('array', BallotItemOption),
+        });
+    }
+}
---
+++ src/app/models/vote/vote.ts
@@ -0,0 +1,17 @@
+import {BaseModel} from '../base-model';
+
+/**
+ * Used as a data wrapper for our ballot model
+ */
+export class Vote extends BaseModel {
+
+    /**
+     * The result recorded
+     */
+    result: number;
+
+    /**
+     * The id of the option this is related to
+     */
+    ballot_item_option_id: number;
+}
